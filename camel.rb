bfc072f6f19cad5a25df40d6a61850872d4235f6
Woojae Lee <woojae.lee@gmail.com>
2015-01-16 09:44:28 -0800

    adding simple amazon search script

---
+++ camel.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'open-uri'
+
+File.open('url.txt', 'r').each_line do |line|
+  line = line.chomp
+  command = "curl --silent -L #{line}|grep -i x-camel-cur-price|grep amazon"
+  reciever = line.gsub(/http\:\/\/camelcamelcamel\.com\//,"").gsub(/\/product.*$/,"")
+  puts "---#{reciever}---"
+  url = %x[ #{command} ]
+  cleaned = url.gsub(/<td style=\"white-space:nowrap;\">\$ <input class=\"price \" type=\"text\" name=\"price\" id=\"dp_amazon\" onfocus=\"change_sub_tab/,"")
+  more_clean = cleaned.gsub(/onkeyup=\"check_desired_price_reality\(this\)\;\" \/>/,"").gsub(/^.*-cur-/,"current-").gsub(/x-camel-/,"")
+  puts more_clean
+  print "\n"
+end
